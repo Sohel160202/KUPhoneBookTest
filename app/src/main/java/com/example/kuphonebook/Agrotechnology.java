c7fc5f762d97872b3d77932408983640fc56b76e
Shiekh Sohel Moon <zohel1602@cseku.ac.bd>
2020-08-09 23:47:12 +0600

    BUG Fixed: Agrotechnology.java New Complete: MBA School Bug Found: Chemistry & Fisheries Layout Missing

--- app/src/main/java/com/example/kuphonebook/Agrotechnology.java
+++ app/src/main/java/com/example/kuphonebook/Agrotechnology.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class Agrotechnology extends AppCompatActivity {
 
+    public String name, number;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/kuphonebook/MBASchool.java
+++ app/src/main/java/com/example/kuphonebook/MBASchool.java
@@ -2,13 +2,40 @@ package com.example.kuphonebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MBASchool extends AppCompatActivity {
+
+    public ImageView BAD, HRM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mbaschool);
+
+        BAD = findViewById(R.id.BAD);
+        HRM = findViewById(R.id.HRM);
+
+        BAD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MBASchool.this, BAD.class);
+                startActivity(intent);
+                return;
+            }
+        });
+
+
+        HRM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MBASchool.this, HRM.class);
+                startActivity(intent);
+                return;
+            }
+        });
     }
 }
